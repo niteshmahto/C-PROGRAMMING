dd74ed6318804d3460430b27357dbc8aa665ab25
Nitesh Khemnarayan Mahto <mahtonitesh46@eng.rizvi.edu.in>
2024-11-18 14:50:27 +0530

    Add files via upload

---
+++ EXPERIMENT 2/Exp 2 task 1.c
@@ -0,0 +1,25 @@
+/*Name: Mahto Nitesh Khemnarayan 
+Class:AI&DS
+Roll no:36
+UIN: 241A044
+/*Aim: WAP to print if entered number is even or odd.*/
+#include<stdio.h>
+int main()
+{
+printf("***Even Odd Finder***");
+int n;
+printf("\nEnter the number:");
+scanf("%d",&n);
+printf("\nEntered number is %d",n);
+if(n%2==0)
+{
+printf("\nEntered number is even");
+}
+else
+printf("\nEntered number is odd");
+return 0;
+}
+/* output
+***Even Odd Finder*** Enter the number:12
+Entered number is 12
+Entered number is even*/
---
+++ EXPERIMENT 2/Exp 2 task 2.c
@@ -0,0 +1,39 @@
+
+/*Name: Mahto Nitesh Khemnarayan 
+Class:AI&DS 
+Roll no:36
+UIN: 241A044
+
+Aim: WAP to find sum of all odd numbers between two numbers entered by user*/
+#include <stdio.h>
+int main() {
+    int start, end, sum = 0;
+
+
+    printf("*** Sum of Odd Numbers ***\n");
+    printf("Enter Start Number: ");
+    scanf("%d", &start);
+
+    printf("Enter Ending Number: ");
+    scanf("%d", &end);
+
+
+    for (int i = start; i <= end; i++) {
+        if (i % 2 != 0) {
+            sum += i;
+        }
+    }
+
+
+    printf("Sum of all the Odd Numbers between %d and %d = %d\n", start, end, sum);
+     return 0;
+
+}
+/* ***Output***
+*** Sum of Odd Numbers ***
+Enter Start Number: 1
+Enter Ending Number: 5
+Sum of all the Odd Numbers between 1 and 5 = 9
+*/
+
+
